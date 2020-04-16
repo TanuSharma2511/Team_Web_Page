99be7d66bb9b5a234c2d6a69e8657b0fe89e9f57
TanuSharma2511 <tanu@post.com>
2020-04-16 18:11:51 +0530

    update commit

--- src/context.js
+++ src/context.js
@@ -6,30 +6,6 @@ export const GlobalContext = createContext();
 //Provider Component
 export const GlobalProvider = (props) => {
     const [transactions , setTransaction] = useState(0);
-
-    // const [state, dispatch] = useReducer(Action, transactions);
-  
-    // Actions
-    // function gram(transaction) {
-    //   dispatch({
-    //     type: 'GRAM',
-    //     payload: transaction
-    //   });
-    // }
-  
-    // function kilogram(transaction) {
-    //   dispatch({
-    //     type: 'KILOGRAM',
-    //     payload: transaction
-    //   });
-    // }
-
-    // function ounce(transaction) {
-    //     dispatch({
-    //       type: 'OUNCE',
-    //       payload: transaction
-    //     });
-    //   }
   
     return (
     <GlobalContext.Provider value={[transactions , setTransaction]}>
